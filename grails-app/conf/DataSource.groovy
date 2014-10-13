ba7dd87e56a65506bfea15a7103dd5c92f5a14be
Franck Silvestre <franck.silvestre@ticetime.com>
2014-10-13 19:57:16 +0200

    Configuration Travis

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -33,9 +33,7 @@ environments {
             driverClassName = "org.postgresql.Driver"
             dialect = org.hibernate.dialect.PostgreSQLDialect
 
-            uri = new URI(System.env.DATABASE_URL)
-            // Pour tester localement :
-            // uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
 
             url = "jdbc:postgresql://" + uri.host + uri.path
             username = uri.userInfo.split(":")[0]
